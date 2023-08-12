c979b3f80ad4cd3aed06b5877770ce440f8d06e7
Henrique Galieta <hgalieta@gmail.com>
2023-08-12 13:42:42 -0300

    create project

---
+++ Envvio.Parking.Api/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace Envvio.Parking.Api
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
