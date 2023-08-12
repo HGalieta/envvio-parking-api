d957ab96677aed19cd202cc4662bdba05d280c1f
Henrique Galieta <hgalieta@gmail.com>
2023-08-12 13:47:10 -0300

    feat: add vehicle model

---
+++ Envvio.Parking.Api/Models/Vehicle.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Envvio.Parking.Api.Models
+{
+    public class Vehicle
+    {
+        public Vehicle(string plate, VehicleType type, ParkingLot parkingLot)
+        {
+            Plate = plate;
+            Type = type;
+            ParkingLot = parkingLot;
+
+            if (Regex.IsMatch(plate, "[A-Z]{3}[0-9][A-Z][0-9]{2}"))
+                Country = "Brasil";
+            if (Regex.IsMatch(plate, "[A-Z]{2}[0-9]{3}[A-Z]{2}"))
+                Country = "Argentina";
+        }
+
+        public string Plate { get; set; }
+        public string Country { get; set; }
+        public VehicleType Type { get; set; }
+        public ParkingLot ParkingLot { get; set; }
+    }
+}
